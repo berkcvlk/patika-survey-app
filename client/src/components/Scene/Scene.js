7c6e9c8a063b7ce5cb1ba0ca3a658f43e2567f4e
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 22:09:42 +0300

    [client] create: scene to add all threejs envs

--- client/src/components/Scene/Scene.js
+++ client/src/components/Scene/Scene.js
@@ -1,5 +1,23 @@
+import { Canvas } from "@react-three/fiber";
+
+import CameraContainer from "./CameraContainer/CameraContainer";
+import Planets from "./Planets/Planets";
+
 const Scene = () => {
-  return <div></div>;
+  return (
+    <Canvas>
+      <CameraContainer>
+        <ambientLight color="#555555" />
+        <pointLight
+          intensity={0.8}
+          power={9}
+          color="#ffebc4"
+          position={[-20, -2, 15]}
+        />
+        <Planets />
+      </CameraContainer>
+    </Canvas>
+  );
 };
 
 export default Scene;
