033b624f3bb5e4da5ff229daf4bafbe3646bbb32
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 22:10:12 +0300

    [client] update: new comps

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import { Header, Scene } from "@components";
+import styles from "./App.module.css";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Scene />
+    </div>
+  );
 };
 
 export default App;
