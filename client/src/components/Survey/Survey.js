54ab10c165d0e59ba0b4495b0549f6ddee3cb1a8
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 22:43:06 +0300

    [client] update: input and state

--- client/src/components/Survey/Survey.js
+++ client/src/components/Survey/Survey.js
@@ -1,7 +1,34 @@
-import React from "react";
+import { inputs } from "@constants/survey";
+import { useState } from "react";
+
+import styles from "./styles.module.css";
 
 const Survey = () => {
-  return <div></div>;
+  const [selected, setSelected] = useState("earth");
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+  };
+
+  return (
+    <div className={styles.survey}>
+      <p className={styles.question}>Choose a planet you want</p>
+      <div className={styles.options}>
+        {inputs.map(({ value, label, ...rest }) => (
+          <label key={value}>
+            <input
+              checked={selected === value}
+              onChange={handleChange}
+              value={value}
+              {...rest}
+            />
+            {label}
+          </label>
+        ))}
+      </div>
+      <button className={styles.button}>Vote Now!</button>
+    </div>
+  );
 };
 
 export default Survey;
---
+++ client/src/constants/survey.js
@@ -0,0 +1,5 @@
+export const inputs = [
+  { name: "planets", value: "earth", type: "radio", label: "Earth" },
+  { name: "planets", value: "mars", type: "radio", label: "Mars" },
+  { name: "planets", value: "jupiter", type: "radio", label: "Jupiter" },
+];
