32169d951e467b33b1137b04f782adb58846ffcc
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 22:08:32 +0300

    [client] create: container to move camera according to mouse coords

--- client/src/components/Scene/CameraContainer/CameraContainer.js
+++ client/src/components/Scene/CameraContainer/CameraContainer.js
@@ -1,5 +1,13 @@
-const CameraContainer = () => {
-  return <div></div>;
+import { useFrame } from "@react-three/fiber";
+
+const CameraContainer = ({ children }) => {
+  // Move camera axis according to mouse pointer coordinates
+  useFrame(({ camera, mouse }, delta) => {
+    camera.position.x -= (mouse.x + camera.position.x) * (delta * 3);
+    camera.position.y -= (mouse.y + camera.position.y) * (delta * 3);
+  });
+
+  return children;
 };
 
 export default CameraContainer;
