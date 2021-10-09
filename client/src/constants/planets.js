71a11285f65564edf70eedacda5a773297c97af4
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 22:06:49 +0300

    [client] create: constants for planets

---
+++ client/src/constants/planets.js
@@ -0,0 +1,32 @@
+export const initials = [
+  {
+    name: "earth",
+    textColor: "#002c77",
+    texture: "earth.jpg",
+    scale: [1, 1, 1],
+  },
+  {
+    name: "mars",
+    textColor: "#c65331",
+    texture: "mars.jpg",
+    scale: [1, 1, 1],
+  },
+  {
+    name: "jupiter",
+    textColor: "#bea88d",
+    texture: "jupiter.jpg",
+    scale: [1.3, 1.3, 1.3],
+  },
+];
+
+export const positions = {
+  left: [-3, -0.75, -4],
+  right: [3, 0, -2],
+  center: [0, 0.75, 0],
+};
+
+export const votes = {
+  earth: 1,
+  mars: 12,
+  jupiter: 3,
+};
