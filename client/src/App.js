5ce115b057e98f40d957f09df1d42f96e1416ecb
Berk Civelek <berkcvlk@gmail.com>
2021-10-09 23:11:44 +0300

    [client] add: socket config

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
+
 import { Header, Scene, Survey } from "@components";
+import { connectSocket, subscribeToNewMessages } from "./socketApi";
 import styles from "./App.module.css";
 
 const App = () => {
+  const [votes, setVotes] = useState({ earth: 0, mars: 0, jupiter: 0 });
+
+  useEffect(() => {
+    connectSocket();
+
+    subscribeToNewMessages((data) => {
+      setVotes(data);
+    });
+  }, []);
+
   return (
     <div className={styles.app}>
       <Header />
-      <Scene />
+      <Scene votes={votes} />
       <Survey />
     </div>
   );
